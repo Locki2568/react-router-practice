5a5fd45ba2c03f08d56d388e68c3011a37e60dcb
Locki Lo <locki.lo@cargosmart.com>
2018-12-06 14:23:11 +0800

    feat: added pages components

--- src/App.js
+++ src/App.js
@@ -1,31 +1,39 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import { Switch, Route, Link } from "react-router-dom";
+import NotFoundPage from './components/NotFoundPage'
+import About from './components/About'
+import Introduction from './components/Introduction'
+
 
 class App extends Component {
   render() {
     return (
-      <Router>
         <div>
-          <ul>
-            <li>
-              <Link to="/about">About</Link>
-            </li>
-          </ul>
-          <Route path="/about" component={About}/>
+          <nav>
+            <ul>
+              <li>
+                <Link to="/">Home</Link>
+              </li>
+              <li>
+                <Link to="/about">About</Link>
+              </li>
+              <li>
+                <Link to="/intro">Intro.</Link>
+              </li>
+            </ul>
+          </nav>
+          <hr></hr>
+          <Switch>
+            <Route path="/" exact component ={()=><h1>home page</h1>}></Route>
+            <Route path="/about" component={About}/>
+            <Route path="/intro" component={Introduction}></Route>
+            <Route component = {NotFoundPage}/>
+          </Switch>
         </div>
-      </Router>
     );
   }
-}
-
-function About() {
-  return (
-    <div>
-      <h2>About</h2>
-    </div>
-  );
 }
 
 export default App;
